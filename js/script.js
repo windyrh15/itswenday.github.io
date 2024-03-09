bf5d22f8ddc28359c9f0460e05ebc34d58003ea2
WindyRH156 <wendaybabang@gmail.com>
2024-03-09 15:49:43 +0700

    Uploading my portofolio

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Ambil element navbar
+const navbarMenu = document.querySelector(".navbar-menu");
+
+// Jika Hamburger diklik
+document
+  .querySelector("#hamburger-menu")
+  .addEventListener("click", function () {
+    navbarMenu.classList.toggle("left-full");
+    navbarMenu.classList.toggle("left-3/4");
+    navbarMenu.classList.toggle("active");
+    navbarMenu.classList.toggle("hidden-sidebar");
+  });
+
+// jika menekan apapun diluar hamburger
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarMenu.contains(e.target)) {
+    navbarMenu.classList.remove("active");
+    navbarMenu.classList.remove("hidden-sidebar");
+    navbarMenu.classList.replace("left-3/4", "left-full");
+  }
+});
