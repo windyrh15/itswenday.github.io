6a18b2280f01778c317094cf3073d04a2c640721
WindyRH156 <wendaybabang@gmail.com>
2024-11-02 11:28:52 +0700

    Nov - 2 - 2024 : updated recent project (JS fix)

--- js/script.js
+++ js/script.js
@@ -13,10 +13,10 @@ document
 // jika menekan apapun diluar hamburger
 const hamburger = document.querySelector("#hamburger-menu");
 
-// document.addEventListener("click", function (e) {
-//   if (!hamburger.contains(e.target) && !navbarMenu.contains(e.target)) {
-//     navbarMenu.classList.remove("active");
-//     navbarMenu.classList.remove("hidden-sidebar");
-//     navbarMenu.classList.replace("left-72", "left-full");
-//   }
-// });
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarMenu.contains(e.target)) {
+    navbarMenu.classList.remove("active");
+    navbarMenu.classList.remove("hidden-sidebar");
+    navbarMenu.classList.toggle("left-[90px]");
+  }
+});
